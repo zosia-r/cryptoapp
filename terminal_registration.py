7d10ba134a881e0265418ef0fda41ce1414da0b8
Jesicca1 <105424817+Jesicca1@users.noreply.github.com>
2025-10-08 14:46:26 +0200

    Added possibility of multiple users

--- terminal_registration.py
+++ terminal_registration.py
@@ -1,13 +1,23 @@
-def terminal_input_register():
+import pandas as pd
+
+# Idea: use pd.DataFrame to store all the information about the users
+# Is probably a bit overkill, so a list/array of dictionaries? or sth similar would probably be sufficient
+user_list = pd.DataFrame(columns=["username", "password"])
+
+
+
+def terminal_register():
 
     loop1 = True
     loop2 = True
 
     #todo: maybe introduce if statements in the loops to check if username/pwd satisfy certain conventions
 
+    username = None
+    pwd = None
+
     while loop1:
         username = input("Please select your username: ")
-        
         loop1 = False
 
     while loop2:
@@ -16,5 +26,11 @@ def terminal_input_register():
 
     print(f"Your username is ", {username})
     print(f"Your password is: ", {pwd_clear})
+    
+    new_user = {"username": username, "password": pwd}
+    global user_list
+    user_list = pd.concat([user_list, pd.DataFrame([new_user])], ignore_index=True)
 
-terminal_input_register()
+    print("You were successfully added as a new user :)")
+
+terminal_register()
