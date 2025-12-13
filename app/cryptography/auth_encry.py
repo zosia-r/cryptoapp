c1a584c5cd0940915cbbde3045fef056be6eca61
Jesicca1 <105424817+Jesicca1@users.noreply.github.com>
2025-12-13 17:28:19 +0100

    Add exr´traction of key from json

--- app/cryptography/auth_encry.py
+++ app/cryptography/auth_encry.py
@@ -1,23 +1,37 @@
 import os
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from app.core import REGISTERED_USERS_PATH
+import json
 
 
 nonce_size = 12
 
 # Todo : not sure what should be used in our case as aad
 # key is the user encryption key, HOW DO I GET IT?
-def encrypt_data(data, key, aad):
+def encrypt_data(data, username, aad):
     nonce = os.random(nonce_size)
     chacha = ChaCha20Poly1305(key)
     encrypted = chacha.encrypt(nonce, data, aad)
     ciphertext = nonce + encrypted
     return ciphertext
 
-def decrypt_data(ciphertext, key, aad):
+
+def decrypt_data(ciphertext, username, aad):
     nonce = ciphertext[:nonce_size]
     encrypted = ciphertext[nonce_size:]
 
+    key = extract_key(username)
     chacha = ChaCha20Poly1305(key)
     plaintext = chacha.decrypt(nonce, encrypted, aad)
 
     return plaintext
+
+
+
+def extract_key(username): 
+
+    with open(REGISTERED_USERS_PATH, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    encryption_salt = data["users"][username]["password"]["encryption_salt"]
+    return encryption_salt
