687e5ef2b0c5042460b3bd408b15af6fc98689b3
Jesicca1 <105424817+Jesicca1@users.noreply.github.com>
2025-12-13 17:38:21 +0100

    Change data storage to store encrypted data

--- app/core/data_storage.py
+++ app/core/data_storage.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from app.core import USERS_DIRECTORY
+from app.cryptography import auth_encry
 
 
 def load_user_data(username: str) -> dict:
@@ -18,26 +19,41 @@ def save_user_data(username: str, data: dict) -> None:
 
 
 def add_income(username: str, amount: float, category: str, date_str: str) -> None:
+    type = "income"
     data = load_user_data(username)["data"]
 
+    # todo: fix datatypes to bytes
+    amount_encry = auth_encry.encrypt_data(amount.encode('utf-8'), username, (username + type).encode('utf-8'))
+    category_encry = auth_encry.encrypt_data(category.encode('utf-8'), username, (username + type).encode('utf-8'))
+    date_encry = auth_encry.encrypt_data(date_str.encode('utf-8'), username, (username + type).encode('utf-8'))
+    timestamp_encry = auth_encry.encrypt_data(datetime.now().isoformat().encode('utf-8'), username, (username + type).encode('utf-8'))
+
+
     data["incomes"].append({
-        "amount": float(amount),
-        "category": category,
-        "date": date_str,
-        "timestamp": datetime.now().isoformat()
+        "amount": amount_encry,
+        "category": category_encry,
+        "date": date_encry,
+        "timestamp": timestamp_encry
     })
 
     save_user_data(username, {"data": data})
 
 
 def add_expense(username: str, amount: float, category: str, date_str: str) -> None:
+    type = "expense"
     data = load_user_data(username)["data"]
 
+    # todo: fix datatypes to bytes
+    amount_encry = auth_encry.encrypt_data(amount.encode('utf-8'), username, (username + type).encode('utf-8'))
+    category_encry = auth_encry.encrypt_data(category.encode('utf-8'), username, (username + type).encode('utf-8'))
+    date_encry = auth_encry.encrypt_data(date_str.encode('utf-8'), username, (username + type).encode('utf-8'))
+    timestamp_encry = auth_encry.encrypt_data(datetime.now().isoformat().encode('utf-8'), username, (username + type).encode('utf-8'))
+
     data["expenses"].append({
-        "amount": float(amount),
-        "category": category,
-        "date": date_str,
-        "timestamp": datetime.now().isoformat()
+        "amount": amount_encry,
+        "category": category_encry,
+        "date": date_encry,
+        "timestamp": timestamp_encry
     })
 
     save_user_data(username, {"data": data})
